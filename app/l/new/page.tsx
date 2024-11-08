f19016b604c23c788b89f8f3bfce2f5bae6153d0
okezieuc <53785400+okezieuc@users.noreply.github.com>
2024-11-07 18:13:07 -0600

    design page for creating communities

--- app/l/new/page.tsx
+++ app/l/new/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Button from "@/components/button";
 import TextArea from "@/components/ui/textarea";
 import TextInput from "@/components/ui/textinput";
+import { useState } from "react";
 
 export default function Page() {
+    const [communityName, setCommunityName] = useState("");
+    const [communityDescription, setCommunityDescription] = useState("");
+
     return <>
         <h1 className="text-3xl mb-16">
             create community
@@ -11,20 +17,17 @@ export default function Page() {
         <div className="flex flex-col gap-12">
             <div>
                 <div className="mb-2">community name</div>
-                <TextInput />
+                <TextInput onChange={(e) => setCommunityName(e.target.value)} />
             </div>
 
             <div>
                 <div className="mb-2">description</div>
-                <TextArea />
+                <TextArea onChange={(e) => setCommunityDescription(e.target.value)} />
             </div>
 
             <div>
                 <Button text="create" />
             </div>
         </div>
-
-
-
     </>;
 }
